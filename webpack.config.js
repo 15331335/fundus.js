ec0a0a1202f92370ed6cdd4e0fa37cf10c33ad65
15331335 <992896924@qq.com>
2019-03-12 12:13:10 +0800

    chore: 添加了 WebAssembly 的构建方法

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,24 @@ module.exports = {
   mode: 'development',
   devServer: {
     contentBase: './dist',
-    clientLogLevel: 'none'
+    clientLogLevel: 'none',
+    before(app) {
+      // use proper mime-type for wasm files
+      app.get('*.wasm', function(req, res, next) {
+        var options = {
+          root: path.resolve(__dirname, 'dist'),
+          dotfiles: 'deny',
+          headers: {
+            'Content-Type': 'application/wasm'
+          }
+        };
+        res.sendFile(req.url, options, function (err) {
+          if (err) {
+            next(err);
+          }
+        });
+      });
+    }
   },
   output: {
     filename: 'fundus.js',
